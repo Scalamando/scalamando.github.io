be8dfe9b86038d58b99baef0badebba848777256
Raimund Canzler <git@rai-canzler.de>
2023-12-22 01:25:57 +0100

    feat: basic layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,27 @@
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: 'Ranade, ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+		},
+		colors: {
+			transparent: "transparent",
+			current: "currentColor",
+			black: "#0A0A0A",
+			white: colors.white,
+			cyan: colors.cyan,
+		},
+		dropShadow: { DEFAULT: "6px 6px 0 rgb(0 0 0 / 0.25)" },
+		extend: {
+			colors: {
+				"brand-discord": "#5865F2",
+				"brand-linkedin": "#0073AF",
+				"brand-github": "#24292F",
+			},
+		},
 	},
 	plugins: [],
 };
