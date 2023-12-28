caa3604f86ce4ca0c4a41ec6326c476eea629ece
Raimund Canzler <git@rai-canzler.de>
2023-12-28 18:01:28 +0100

    feat: add astro-compress

--- astro.config.ts
+++ astro.config.ts
@@ -1,41 +1,41 @@
 import { defineConfig } from "astro/config";
 import fs from "node:fs";
-
 import Icons from "unplugin-icons/vite";
 import tailwind from "@astrojs/tailwind";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind({ configFile: "tailwind.config.ts" })],
-	site: "https://rai-canzler.de",
-	vite: {
-		ssr: {
-			external: ["@resvg/resvg-js"],
-		},
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-		plugins: [
-			Icons({
-				compiler: "astro",
-				scale: 1.25,
-				iconCustomizer(collection, _icon, props) {
-					if (collection === "tabler") {
-						props.width = "24px";
-						props.height = "24px";
-					}
-				},
-			}),
-			{
-				name: 'woff-loader',
-				transform(_code, id) {
-					if(!id.match(/.woff$/)) {
-						return null;
-					}
-					const data = fs.readFileSync(id);
-					return `export default new Uint8Array([${[...data.values()]}]);`;
-				}
-			}
-		],
-	},
+  integrations: [tailwind({
+    configFile: "tailwind.config.ts"
+  }), compress()],
+  site: "https://rai-canzler.de",
+  vite: {
+    ssr: {
+      external: ["@resvg/resvg-js"]
+    },
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"]
+    },
+    plugins: [Icons({
+      compiler: "astro",
+      scale: 1.25,
+      iconCustomizer(collection, _icon, props) {
+        if (collection === "tabler") {
+          props.width = "24px";
+          props.height = "24px";
+        }
+      }
+    }), {
+      name: 'woff-loader',
+      transform(_code, id) {
+        if (!id.match(/.woff$/)) {
+          return null;
+        }
+        const data = fs.readFileSync(id);
+        return `export default new Uint8Array([${[...data.values()]}]);`;
+      }
+    }]
+  }
 });
